919646c99a25788a7c82f79c07ce4252ce8661d8
Tomás Antunes <33696484+tsadev@users.noreply.github.com>
2018-05-13 00:53:11 +0100

    13/05/2018
    - Added more 4 questions

--- millionare.py
+++ millionare.py
@@ -1,6 +1,6 @@
 # millionare.py (By: Tomás A.)
 
-# This game has a total of 4 questions, with multiple answers (more to come)
+# This game has a total of 8 questions, with multiple answers (more to come)
 
 import time
 import random
@@ -17,11 +17,10 @@ print("The first prize, for a correct answer is $1,000 and it goes all the way u
 
 time.sleep(1)
 
-#num = random.randint(1,2)
-num = 1
+num = input("Choose a number beteween 1 and 2: ")
 
 # first question
-if num == 1:
+if num == "1":
     print("The first question, for $1000, will be...")
     time.sleep(random.randint(1,2))
     print("Which of these former U.S. Presidents appeared on the television series Laugh-In?")
@@ -35,9 +34,26 @@ if num == 1:
         print(myName, "bank: $", playerBank)
     else:
         print("Your answer is wrong! You will do better next time! See you!")
+elif num == "2":
+    print("The first question, for $1000, will be...")
+    time.sleep(random.randint(1,2))
+    print("What letter must appear at the beginning of the registration number of all non-military aircraft in the U.S.?")
+    print("Possible answers: a) N; b) A; c) U; d) L")
+    firstAnswer = input("Your choice: ")
+    print("Your choice is...")
+    time.sleep(random.randint(1,4))
+    if firstAnswer == "a":
+        print("Cooooorect! You jsut maid $1.000!")
+        playerBank = playerBank + 1000
+        print(myName, "bank: $", playerBank)
+    else:
+        print("Your answer is wrong! You will do better next time! See you!")
+
+
+time.sleep(random.randint(1,3))
 
 # second question
-if playerBank > 0 and num == 1:
+if playerBank > 0 and num == "1":
     print("The sencond question, for $5000, will be...")
     time.sleep(random.randint(1,2))
     print("The Earth is approximately how many miles away from the Sun?")
@@ -51,9 +67,25 @@ if playerBank > 0 and num == 1:
         print(myName, "bank: $", playerBank)
     else:
         print("Your answer is wrong! You will do better next time! See you!")
+elif playerBank > 0 and num == "2":
+    print("The sencond question, for $5000, will be...")
+    time.sleep(random.randint(1,2))
+    print("In the children's book series, where is Paddington Bear originally from?")
+    print("Possible answers: a) India; b) Peru; c) Canada; d) Iceland")
+    secondAnswer = input("Your choise: ")
+    print("Your choice is...")
+    time.sleep(random.randint(1,4))
+    if secondAnswer == "b":
+        print("Cooooorect! You jsut maid $5.000!")
+        playerBank = playerBank + 5000
+        print(myName, "bank: $", playerBank)
+    else:
+        print("Your answer is wrong! You will do better next time! See you!")
+
+time.sleep(random.randint(1,3))
 
 # third question
-if playerBank > 1000 and num == 1:
+if playerBank > 1000 and num == "1":
     print("The third question, for $100.000, will be...")
     time.sleep(random.randint(1,2))
     print("Which insect shorted out an early supercomputer and inspired the term «computer bug»?")
@@ -67,10 +99,16 @@ if playerBank > 1000 and num == 1:
         print(myName, "bank: $", playerBank)
     else:
         print("Your answer is wrong! You will do better next time! See you!")
+elif playerBank > 1000 and num == "2":
+    print("The third question, for $100.000, will be...")
+    time.sleep(random.randint(1,2))
+    print("«Nephelococcygia» is the practice of doing what?")
+    print("Posible answers: a) Finding shapes in clouds; b) Sleeping with your eyes open; c) Breaking glass with your voice; d) Swimming in freezing water")
+
+time.sleep(random.randint(1,3))
 
 # fourth question
-
-if playerBank > 6000 and num == 1:
+if playerBank > 6000 and num == "1":
     print("The fourth question, for $10.000.000, will be...")
     time.sleep(random.randint(1,2))
     print("Which of the following landlocked countries is entirely contained within another country?")
@@ -84,7 +122,22 @@ if playerBank > 6000 and num == 1:
         print(myName, "bank: $", playerBank)
     else:
         print("Your answer is wrong! You will do better next time! See you!")
+elif playerBank > 6000 and num == "1":
+    print("The fourth question, for $10.000.000, will be...")
+    time.sleep(random.randint(1,2))
+    print("The U.S. icon «Uncle Sam» was based on Samuel Wilson, who worked during the War of 1812 as a what?")
+    print("Possible answers: a) Meat inspector; b) Mail delieverer; c) Historian; d) Weapons mechanic")
+    fourthAnswer = input("Your choice: ")
+    print("Your choice is...")
+    time.sleep(random.randint(1,4))
+    if fourthAnswer == "a":
+        print("Cooooorect! You jsut maid $100000!")
+        playerBank = playerBank + 10000000
+        print(myName, "bank: $", playerBank)
+    else:
+        print("Your answer is wrong! You will do better next time! See you!")
 
+time.sleep(random.randint(1,3))
 
 # victory
 if playerBank == 10106000:  # max on bank
