5810fff2769266e83ee559cb2a4a45ace3f3d5e0
Tomás Antunes <33696484+tsadev@users.noreply.github.com>
2018-05-17 13:58:27 +0100

    Needed to change repository location also website shit for my paps company

--- website/main.py
+++ website/main.py
@@ -1,12 +1,27 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__) 
 
 @app.route('/')
-
+@app.route('/home')
 def home():
+    """Renders the home page."""
+    return render_template(
+        'home.html',
+        title='Home Page',
+        year=datetime.now().year,
+    )
 
-    return render_template("home.html")
+@app.route('/about')
+def about():
+    """Renders the about page."""
+    return render_template(
+        'about.html',
+        title='About',
+        year=datetime.now().year,
+        message='Your application description page.'
+    )
 
 if __name__ == '__main__':
 
